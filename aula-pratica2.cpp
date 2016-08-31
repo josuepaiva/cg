94af8021f0dd8661e342ecac5d66cd6ef6a84831
Josué <do-paiva@hotmail.com>
2016-08-31 15:14:49 -0300

    tarefa 3 imcompleta

--- aula-pratica2.cpp
+++ aula-pratica2.cpp
@@ -5,23 +5,48 @@
 float rotate = 0;
 float x = 0, y = 0, z = 0;
 
+
+void init(){
+
+	glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluOrtho2D(-150,150, -150, 150);
+}
+
 void display(){
 	
-	glClear(GL_COLOR_BUFFER_BIT);
-	glRotated(1,0,0,rotate);0	
-	glTranslated(0,-0.2,0);
+	glClear(GL_COLOR_BUFFER_BIT);	
 	//glScale()
-	glBegin(GL_TRIANGLES);
-	 glColor3d(1,1,0.5);
-	 glVertex3d(-1,0,0);
-    	 glVertex3d(1,0,0);
-	 glVertex3d(0,1,0);
-         
-         /*glVertex3d(-1,0,0);
-	 glVertex3d(1,0,0);
-	 glVertex3d(0,-1,0);*/
-	
+	glBegin(GL_QUADS);
+	 glColor3d(1,1,1);
+	 glVertex3i(-150,-150,0);
+    	 glVertex3i(-150,0,0);
+	 glVertex3i(0,0,0);
+	 glVertex3i(0,-150,0);	
  	glEnd();
+
+	
+	glBegin(GL_QUADS);
+	 glColor3d(1,1,1);
+	 glVertex3i(0,0,0);
+	 glVertex3i(0,150,0);
+	 glVertex3i(150,150,0);
+	 glVertex3i(150,0,0);
+	glEnd();
+
+	glPushMatrix();
+	//glRotated(rotate,0,0,1);
+	glTranslated(x,y,z);
+	glBegin(GL_TRIANGLES);
+	 glColor3d(1,0,0);
+	 glVertex3d(-50, -50, 1);
+	 glColor3d(0,1,0);
+	 glVertex3d(50,-50, 1);
+	 glColor3d(0,0,1);
+	 glVertex3d(0,50, 1);
+ 	glEnd();
+
+	glPopMatrix();
 
 	glFlush();
 
@@ -35,6 +60,7 @@ void keyboard(unsigned char key, int a, int b){
 			break;
 		case 'c':
 			rotate+= 10;
+			glutPostRedisplay();
 			std::cout<<"rotate"<<std::endl;
 			break;
 		case 'r':
@@ -46,8 +72,7 @@ void keyboard(unsigned char key, int a, int b){
 		case 'b':
 			glClearColor(0.0,0.0,1.0,1.0);
 			break;
-		case 'f':
-			glutFullScreen();
+		default:
 			break;		
 	}
 
@@ -58,24 +83,27 @@ void keyspecials(int tecla, int x, int y){
 	
 	switch(tecla){
 		 case GLUT_KEY_UP:
-                        y+= 0.2;
+                        y+= 2;
                 std::cout<<"Tecla cima presionada"<<std::endl;
                         break;
                 case GLUT_KEY_LEFT:
 		std::cout<<"Tecla esquerda presionada"<<std::endl;
-                        x+= 0.1;
+                        x+= 2;
                         break;
+		default:
+			break;
 	}
 }
 int main(int argc, char** argv){
 
 	glutInit(&argc, argv);
    	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-   	glutInitWindowSize (500, 300); 
+   	glutInitWindowSize (500, 500); 
    	glutInitWindowPosition (100, 100);
    	glutCreateWindow ("Aula-Pratica 2");
 	glutKeyboardFunc(keyboard);
 	glutSpecialFunc(keyspecials);
+	init();
    	glutDisplayFunc(display); 
    	glutMainLoop();
 
