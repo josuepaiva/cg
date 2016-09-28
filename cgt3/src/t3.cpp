1b4e117f5f3d718cde42372d8fcf694e26ea61d6
josuepaiva <josuepaiva@lavid.ufpb.br>
2016-09-28 20:39:40 -0300

    Update t3.cpp

--- cgt3/src/t3.cpp
+++ cgt3/src/t3.cpp
@@ -10,38 +10,6 @@
  
 int main(){
 	
-/*	ListaVertice *listaVertice;
-	ListaFace *listaFace;
-	
-	listaVertice = new ListaVertice();
-	listaFace = new ListaFace();
-	FILE * arquivo;
- 	
-	char c[100], j;
-	float x, y, z;
-	int i = 0, auxv = 0;
-
-    arquivo = fopen("cubo.obj","r");
- 
-    while(fgets(c,121,arquivo)){
-            int n = sscanf(c,"%c %f %f %f",&j,&x,&y,&z);
-        if( (c[0]!='u' && c[0] != 's') && i == 4){
-                if(c[0] == 'v'){
-					Vertice vertice(x,y,z);
-                    listaVertice->insere(vertice);
-                }
-                else{
-                    if(c[0] =='f'){
-                        Face face(x,y,z);
-						listaFace->insere(face);
-                    }
-                }
-        }
-        if(i<4){
-        i++;
-        }
- 
-    }*/
 	Importador *import = new Importador();
 	import->ler("cubo.obj");
 	import->printa();
